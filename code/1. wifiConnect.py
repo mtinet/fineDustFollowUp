068b203f91f6b38b0de9bd38d77496c3fe61a9ca
Jason Juhyun Kim <mtinet@hanmail.net>
2023-09-17 23:41:20 +0900

    Update 1. wifiConnect.py

--- code/1. wifiConnect.py
+++ code/1. wifiConnect.py
@@ -11,7 +11,7 @@ def wifiConnect():
     wlan.active(True)
     if not wlan.isconnected():
         # 와이파이 연결하기
-        wlan.connect(SSID, password)  # 4, 5번 줄에 입력한 SSID와 password가 입력됨
+        wlan.connect(SSID, password)  # 5, 6번 줄에 입력한 SSID와 password가 입력됨
         print("Waiting for Wi-Fi connection", end="...")
         print()
         while not wlan.isconnected():
